8acc7ee18103aab74a0dfd32006cfa4230a04a72
Priske <eeckman_ben@hotmail.com>
2026-07-29 14:24:43 +0200

    Adapted Description Length & Title change

---
+++ PersonalSite.Api/Migrations/20260729104038_IncreaseProjectDescriptionLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalSite.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseProjectDescriptionLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
